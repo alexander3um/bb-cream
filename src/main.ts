53f6802aa3b059415f7f169a2b4c4cc202f1dcb1
Alexander Petrov <alexandr3um@gmail.com>
2024-06-09 01:05:07 +0300

    Add boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import "./style.css";
+import { process } from "./process/process.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="box">
@@ -10,3 +11,20 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </div>
   </div> 
 `;
+
+const from = document.querySelector<HTMLTextAreaElement>(".box-from textarea");
+const to = document.querySelector<HTMLTextAreaElement>(".box-to textarea");
+
+if (!from || !to) {
+  throw new Error(
+    "Congratulations! You're the lucky winner and your prize is бан",
+  );
+}
+
+from.addEventListener("change", (event) => {
+  const target = event.target as HTMLTextAreaElement;
+
+  if (target) {
+    to.value = process(target.value);
+  }
+});
---
+++ src/process/process.ts
@@ -0,0 +1,5 @@
+export const process = (input: string): string => {
+  console.log(input);
+
+  return input;
+};
