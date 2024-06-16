05007269d90a166e08939095e9468b0790656ff8
Alexander Petrov <alexandr3um@gmail.com>
2024-06-16 05:26:08 +0300

    Add render

--- src/process/render/render.ts
+++ src/process/render/render.ts
@@ -4,13 +4,36 @@ import { sortVersions } from "./sortVersions.ts";
 export const render = (versions: Version[]): string => {
   const sortedVersions = sortVersions(versions);
 
-  return sortedVersions
-    .map((version) => {
-      const versionBbCode = `[v]${version.version}[/v]`;
+  const groupedVersions: Record<string, string> = {};
 
-      // const download = version.linkType === "attachment" ? `[attachment=${version.link}]` : ``;
+  for (const version of sortedVersions) {
+    const attachmentLine =
+      version.linkType === "attachment"
+        ? `[attachment=${version.link}]`
+        : `[url=${version.link}]${version.link}[/url]`;
 
-      return `[vers]${versionBbCode}[files][${version.loader}][/${version.loader}][/files][/vers]`;
-    })
-    .join("\n\n");
+    const dependencies = version.dependencies
+      .map((dependency) => `[url=${dependency.link}]${dependency.name}[/url]`)
+      .join(" и ");
+
+    const dependenciesLine =
+      version.dependencies.length > 0
+        ? `[dep]требуется установить ${dependencies}[/dep]`
+        : "";
+
+    groupedVersions[version.version] =
+      groupedVersions[version.version] ??
+      "" +
+        `[${version.loader}]${attachmentLine}${dependenciesLine}[/${version.loader}]`;
+  }
+
+  const codes: string[] = [];
+
+  for (const version of Object.keys(groupedVersions)) {
+    codes.push(
+      `[vers][v]${version}[/v][files]${groupedVersions[version]}[/files][/vers]`,
+    );
+  }
+
+  return codes.join("\n\n");
 };
--- src/process/render/sortVersions.ts
+++ src/process/render/sortVersions.ts
@@ -1,30 +1,32 @@
 import { Version } from "../../types.ts";
 
 export const sortVersions = (versions: Version[]): Version[] =>
-  [...versions].sort((a, b) => {
-    const pa = a.version.split(".");
-    const pb = b.version.split(".");
+  [...versions]
+    .sort((a, b) => {
+      const pa = a.version.split(".");
+      const pb = b.version.split(".");
 
-    for (let i = 0; i < 3; i++) {
-      const na = Number(pa[i]);
-      const nb = Number(pb[i]);
+      for (let i = 0; i < 3; i++) {
+        const na = Number(pa[i]);
+        const nb = Number(pb[i]);
 
-      if (na > nb) {
-        return 1;
+        if (na > nb) {
+          return 1;
+        }
+
+        if (nb > na) {
+          return -1;
+        }
+
+        if (!isNaN(na) && isNaN(nb)) {
+          return 1;
+        }
+
+        if (isNaN(na) && !isNaN(nb)) {
+          return -1;
+        }
       }
 
-      if (nb > na) {
-        return -1;
-      }
-
-      if (!isNaN(na) && isNaN(nb)) {
-        return 1;
-      }
-
-      if (isNaN(na) && !isNaN(nb)) {
-        return -1;
-      }
-    }
-
-    return 0;
-  });
+      return 0;
+    })
+    .reverse();
